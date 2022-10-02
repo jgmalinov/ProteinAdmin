bfa87a4c4b36dcb15b3163f54a3aadc6e2683954
jgmalinov <gogotkd24@gmail.com>
2022-10-02 20:12:41 +0300

    added animated logo and basic login view

--- src/Animations/animations.js
+++ src/Animations/animations.js
@@ -1,22 +1,50 @@
 export function pizzaEatingAnimation() {
     const sliceIds = ['slice1', 'slice2', 'slice3', 'slice4', 'slice5', 'slice6'];
-    const playFunctions = []
+    const playFunctions = [];
+    let delay = 500;
+    let angle = 0;
+    let duration = 3000;
+    const transformOrigin = 'transform-origin: bottom left';
+
     for (let i=0; i < sliceIds.length; i++) {
+        let transform = `translate(0%, 100%) rotate(${angle}deg) skew(6.5deg, 6.5deg)`; 
         const pizzaSlice = document.getElementById(sliceIds[i]);
         const keyframes = new KeyframeEffect(
             pizzaSlice,
-            [{transform: 'scale(1)'}, {transform: 'scale(0.1)'}, {transform: 'scale(1)'}],
-            {duration: 1000, iterations: 5}
+            [{transform: transform , 'transform-origin': transformOrigin, opacity: '1'}, {transform: transform, 'transform-origin': transformOrigin, opacity: '0'}],
+            {duration: duration, iterations: 1, delay: delay, easing: "cubic-bezier(0, 0, 0, 6)"}
         );
         const animation = new Animation(keyframes, document.timeline);
+        animation.onfinish = () => {console.log(`Ending at: ${Date.now()}`)};
         function play() {
             animation.play();
+            console.log(`Playing at: ${Date.now()}`);
         }
-        playFunctions.push(play);
+        playFunctions.push({play});
+        angle += 60;
+        delay += 500;
+        duration -= 500;
     };
     
-    for (let i=0; i < playFunctions.length; i++) {
-        const playFunction = playFunctions[i];
-        playFunction();
-    }
+    function animationFrames() {
+        for (let i=0; i < playFunctions.length; i++) {
+            if (!playFunctions[0].timestamp) {
+                playFunctions[0].timestamp = Date.now();
+                playFunctions[0].first = true;
+            };
+        }
+        if (((Date.now() - playFunctions[0].timestamp) >= 3550) || playFunctions[0].first) {
+            playFunctions[0].timestamp = Date.now();
+            for (let i=0; i < playFunctions.length; i++) {
+                playFunctions[i].play();
+                /* console.log(`playin ${i}`); */
+            };
+            if (playFunctions[0].first) {
+                playFunctions[0].first = false;
+            }
+        };
+        window.requestAnimationFrame(animationFrames);
+    };      
+    
+    window.requestAnimationFrame(animationFrames);
 };
--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -1,12 +1,26 @@
 export function Login(args) {
     return (
-        <div>
-            <i className="fa-solid fa-pizza-slice" id="slice1"></i>
-            <i className="fa-solid fa-pizza-slice" id="slice2"></i>
-            <i className="fa-solid fa-pizza-slice" id="slice3"></i>
-            <i className="fa-solid fa-pizza-slice" id="slice4"></i>
-            <i className="fa-solid fa-pizza-slice" id="slice5"></i>
-            <i className="fa-solid fa-pizza-slice" id="slice6"></i>
+        <div id="login">
+            <section id="loginSection">
+                <h1>Protein Admin</h1>
+                <form id="loginForm">
+                    <input placeholder="username"></input>
+                    <input placeholder="password"></input>
+                    <button type="submit">Login!</button>
+                    <button type="submit">Sign in with Google</button>
+                    <a id="LinkToRegister" href="/">Don't have an account? Register here!</a>
+                </form>
+            </section>
+
+            <section id="logo">
+                <i className="fa-solid fa-pizza-slice" id="slice1"></i>
+                <i className="fa-solid fa-pizza-slice" id="slice2"></i>
+                <i className="fa-solid fa-pizza-slice" id="slice3"></i>
+                <i className="fa-solid fa-pizza-slice" id="slice4"></i>
+                <i className="fa-solid fa-pizza-slice" id="slice5"></i>
+                <i className="fa-solid fa-pizza-slice" id="slice6"></i>
+            </section>
+
         </div>
     )
 }
